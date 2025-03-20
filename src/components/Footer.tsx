5395b20a44c44a47edb385f0aa9f16259ff80777
jacky2252v <vivekbaraiya000@gmail.com>
2025-03-20 16:51:54 +0530

    Footer component added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="bg-gray-400/15 text-white p-1 px-6 flex justify-between items-center">
+            <div>
+                <p>&copy; 2025 Krisha Bhumi</p>
+            </div>
+            <div>
+                <p style={{ fontFamily: "Moontime", fontSize:"25px" }}>Viv.B</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
