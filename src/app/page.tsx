8f38e7021111f74c26621e7091d0f05549663e3f
jacky2252v <vivekbaraiya000@gmail.com>
2025-03-26 17:23:06 +0530

    Refactor global styles and layout structure; integrate theme provider and Toaster component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
-'use client'
+"use client"
 
-import Footer from "../components/Footer";
-import Hero from "../components/Hero";
-import Navbar from "../components/Navbar";
-import WhatWeDo from "../components/whatWeDo";
+import Footer from "../components/Footer"
+import GetInTouch from "../components/GetInTouch"
+import Hero from "../components/Hero"
+import Navbar from "../components/Navbar"
+import WhatWeDo from "../components/whatWeDo"
+
 
 export default function Home() {
   return (
-    <div>
+    <main className="min-h-screen bg-white dark:bg-gray-950 text-gray-900 dark:text-gray-100">
       <Navbar />
       <Hero />
       <WhatWeDo />
+      <GetInTouch />
       <Footer />
-    </div>
-  );
+    </main>
+  )
 }
+
