26b878fe33d150ded2dfa9dc04865b5d0856f18e
jacky2252v <vivekbaraiya000@gmail.com>
2025-04-01 12:18:31 +0530

    Refactor layout and structure: integrate ThemeProvider and Toaster, update imports, and enhance main layout styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
-'use client'
+"use client"
 
-import Footer from "../components/Footer";
-import GetInTouch from "../components/GetInTouch";
-import Hero from "../components/Hero";
-import Navbar from "../components/Navbar";
-import WhatWeDo from "../components/whatWeDo";
+import Footer from "@/components/Footer"
+import GetInTouch from "@/components/GetInTouch"
+import Hero from "@/components/Hero"
+import Navbar from "@/components/Navbar"
+import WhatWeDo from "@/components/whatWeDo"
+
+
 
 export default function Home() {
   return (
-    <div>
+    <main className="min-h-screen bg-white dark:bg-gray-950 text-gray-900 dark:text-gray-100">
       <Navbar />
       <Hero />
       <WhatWeDo />
       <GetInTouch />
       <Footer />
-    </div>
-  );
+    </main>
+  )
 }
+
